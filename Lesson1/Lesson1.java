60679483ad7a8d76bbfd583b11709e15400e77b2
DarjaElina <daria.elina98@gmail.com>
2025-05-08 19:57:30 +0300

    update .gitignore

--- Lesson1/Lesson1.java
+++ Lesson1/Lesson1.java
@@ -23,5 +23,14 @@ public class Lesson1 {
         System.out.println("abcdefg");
         System.out.println();
         System.out.println("hijklmn");
+
+        // Lesson 1 Homework
+
+        System.out.println("                Rubaiyat             ");
+        System.out.println("Into this Universe, and why not knowing");
+        System.out.println("Nor whence, like Water willy-nilly flowing:");
+        System.out.println("And out of it, as Wind along the Waste,");
+        System.out.println("I know not whither, willy-nilly blowing.");
+        System.out.println("                            Omar Khayyam");
     }
 }
