05ee628eb72e1c63ce0e4a1545ea8040d4a81726
DarjaElina <daria.elina98@gmail.com>
2025-05-08 22:06:18 +0300

    add homework folder

---
+++ Lesson2/Lesson2.java
@@ -0,0 +1,5 @@
+class Lesson2 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ homework/Lesson1.java
@@ -0,0 +1,12 @@
+public class Lesson1 {
+
+    // Lesson 1 Homework
+    public static void main(String[] args) {
+        System.out.println("                Rubaiyat");
+        System.out.println("Into this Universe, and why not knowing");
+        System.out.println("Nor whence, like Water willy-nilly flowing:");
+        System.out.println("And out of it, as Wind along the Waste,");
+        System.out.println("I know not whither, willy-nilly blowing.");
+        System.out.println("                            Omar Khayyam");
+    }
+}
