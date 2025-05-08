41e9b43d98f3f680e8c06a0ea67e69b54fd5c5cd
DarjaElina <daria.elina98@gmail.com>
2025-05-08 19:10:44 +0300

    Lesson 1: print and println methods

---
+++ Lesson1/Lesson1.java
@@ -0,0 +1,27 @@
+public class Lesson1 {
+
+    // this is the main method
+    // main is a starting point to execute any java program
+    // without main we cannot run java program
+    public static void main(String[] args) {
+        // everything inside this curly braces will be executed
+        System.out.println("Welcome to the first lesson!");
+        System.out.println("Hello Java!");
+
+        System.out.print("My name is ");
+        System.out.println("Daria");
+
+        System.out.print("A ");
+        System.out.println("a");
+
+        System.out.print("B ");
+        System.out.println("b");
+
+        System.out.print("C ");
+        System.out.println("c");
+
+        System.out.println("abcdefg");
+        System.out.println();
+        System.out.println("hijklmn");
+    }
+}
