0c8cd527d482b59482870d06a22d469c072f75a6
DarjaElina <daria.elina98@gmail.com>
2025-05-09 12:29:59 +0300

    move homework 1 instructions outside the class for consistency

--- src/Homework/Lesson1.java
+++ src/Homework/Lesson1.java
@@ -1,8 +1,9 @@
 package Homework;
 
+// Lesson 1 Homework: Practice print and println methods.
+
 public class Lesson1 {
 
-    // Lesson 1 Homework
     public static void main(String[] args) {
         System.out.println("                Rubaiyat");
         System.out.println("Into this Universe, and why not knowing");
