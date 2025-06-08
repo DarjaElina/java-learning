9105c546776111df05f07de35614a3f7df8b2843
DarjaElina <daria.elina98@gmail.com>
2025-06-08 19:13:03 +0300

    add Lesson 16 part 2 code examples

---
+++ src/Lesson16Part2/Main.java
@@ -0,0 +1,29 @@
+package Lesson16Part2;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        System.out.println(s1.isBlank()); // false
+
+        String s2 = "     ";
+        System.out.println(s2.isBlank()); // true
+
+        String s3 = "    g ";
+        System.out.println(s3.isBlank()); // false
+
+        String s4 = " ";
+        System.out.println(s4.isEmpty()); // false
+
+        String s5 = "";
+        System.out.println(s5.isEmpty()); // true
+
+        String s6 = "       Hello   ";
+        System.out.println(s6.strip());
+        System.out.println(s6.stripLeading());
+        System.out.println(s6.stripTrailing());
+
+        String s7 = "Goodbye";
+        String s8 = s7.strip();
+        System.out.println(s7 == s8);
+    }
+}
