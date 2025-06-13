d08cff36d6ac9ea773f96b190175528f14986bc0
DarjaElina <daria.elina98@gmail.com>
2025-06-13 19:46:12 +0300

    add Lesson 17 homework version 2

--- src/Homework/Lesson17/Main.java
+++ src/Homework/Lesson17/Main.java
@@ -5,11 +5,29 @@ public class Main {
         return a.toString().equals(b.toString());
     }
 
+    public static boolean equals2(StringBuilder a, StringBuilder b) {
+        boolean result = true;
+        if (a.length() == b.length()) {
+            for (int i = 0; i < a.length(); i++) {
+                if (a.charAt(i) != b.charAt(i)) {
+                    result = false;
+                    break;
+                }
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         StringBuilder a = new StringBuilder("Hello");
         StringBuilder b = new StringBuilder("World");
         StringBuilder c = new StringBuilder("Hello");
         System.out.println(equals(a, b));
         System.out.println(equals(a, c));
+
+        System.out.println(equals2(a, b));
+        System.out.println(equals2(a, c));
     }
 }
