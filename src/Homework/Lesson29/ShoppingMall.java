3b2e30220940ae8f3b80ffb6c96d8563f762da84
DarjaElina <daria.elina98@gmail.com>
2025-06-25 17:49:13 +0300

    add Lesson 29 Homework

---
+++ src/Homework/Lesson29/ShoppingMall.java
@@ -0,0 +1,28 @@
+package Homework.Lesson29;
+import java.time.*;
+import java.time.format.*;
+
+public class ShoppingMall {
+    static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy, MMMM-MM !! HH:mm");
+    static DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern(" HH:mm, MM/MMM/dd");
+
+    void shift(LocalDateTime ldt1, LocalDateTime ldt2, Period period, Duration duration) {
+        while (ldt1.isBefore(ldt2)) {
+            System.out.println("We are working from: " + ldt1.format(formatter));
+            ldt1 = ldt1.plus(period);
+            System.out.println("To: " + ldt1.format(formatter2));
+            System.out.println("We are closed from: " + ldt1.format(formatter));
+            ldt2 = ldt2.plus(duration);
+            System.out.println("To: " + ldt2.format(formatter2));
+        }
+    }
+
+    public static void main(String[] args) {
+        ShoppingMall shoppingMall = new ShoppingMall();
+        LocalDateTime ldt1 = LocalDateTime.of(2025, Month.FEBRUARY, 1, 0, 0, 0);
+        LocalDateTime ldt2 =  LocalDateTime.of(2025, Month.MAY, 1, 0, 0, 0);
+        Period period = Period.ofWeeks(1);
+        Duration duration = Duration.ofHours(24);
+        shoppingMall.shift(ldt1, ldt2, period, duration);
+    }
+}
